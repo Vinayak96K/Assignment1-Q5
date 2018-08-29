98a0a30a44cbb56b3ba0b9176bcb0d9ba3a96ddf
Vinayak96k <vinayak.patil0304@gmail.com>
2018-08-29 15:27:56 +0530

    first commit

---
+++ Helper.c
@@ -0,0 +1,19 @@
+#include"MyHeader.h"
+
+//////////////////////////////////////////////////////////////////////////////////////
+//  Function Name: Accept
+//  Description: Accepts one number from user and prints number of * on the screen.
+//  Input: int[IN]
+//  Output: NA
+//  Author: Vinayak Mahendra Patil
+/////////////////////////////////////////////////////////////////////////////////////
+
+void Accept(int iNo)
+{
+    int iCnt=0;
+    for(iCnt=1;iCnt<=iNo;iCnt++)
+    {
+        printf("*\t");
+    }
+
+}
